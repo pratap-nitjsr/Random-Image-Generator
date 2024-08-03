3e7d4cc00c8f49eca31ea8f8036f4d1bb76733c5
pratap-nitjsr <127110382+pratap-nitjsr@users.noreply.github.com>
2024-08-04 01:30:17 +0530

    Update generate_images.py

--- Backend/Flask/FlaskBackend/generate_images.py
+++ Backend/Flask/FlaskBackend/generate_images.py
@@ -51,5 +51,6 @@ def generate():
     return jsonify(images=base64_images)
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
