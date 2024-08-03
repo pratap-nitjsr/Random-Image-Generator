17a919571e866b3b754e03491994d8376c26ecf1
pratap-nitjsr <127110382+pratap-nitjsr@users.noreply.github.com>
2024-08-04 02:13:50 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/',
+  baseURL: 'https://random-image-generator-q86a.onrender.com/',
 });
 
 export default api;
