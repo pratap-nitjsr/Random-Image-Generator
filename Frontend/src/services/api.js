9d6c49497653ee4034faeecef0d06bd422550830
Pratap Kumar <127110382+pratap-nitjsr@users.noreply.github.com>
2025-02-01 21:19:09 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/',
+  baseURL: 'https://random-image-generator-wr8i.vercel.app/',
 });
 
 export default api;
